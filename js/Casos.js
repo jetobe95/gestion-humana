b96e7e6946e6f393cf8438e1e45c98f6c59219e7
Jeffrey <alto.951@hotmail.com>
2018-04-28 14:26:20 -0500

    Agregando el contador de casos

---
+++ js/Casos.js
@@ -0,0 +1,42 @@
+var CasosRef = firebase.database().ref('Casos');
+var casosAbierto=0,casoCerrados=0,TodosLoscasos=[],InfoCaso;
+/**
+ * Obejeto del tipo caso
+ */
+class Casos{
+
+    constructor(Param) {
+        var Data = Param.val();
+        this.Id_Caso=Param.key;
+        this.Brigadista_Encargado=Data.Brigadista_Encargado;
+        this.Descripcion=Data.Descripcion;
+        this.Fecha_Caso=Data.Fecha_Caso;
+        this.Feedback=Data.Feedback;
+        this.Prioridad_Caso=Data.Prioridad_Caso;
+        this.Resuelto=Data.Resuelto;
+        this.Ubicacion_Caso=Data.Ubicacion_Caso;
+        
+    
+      }
+}
+
+CasosRef.once('value', function (data) {
+
+    data.forEach(caso => {
+        var CheckCaso=new Casos(caso);
+        TodosLoscasos.push(new Casos(caso))
+         InfoCaso=caso;
+         if (CheckCaso.Resuelto=="Exito") {
+             casoCerrados++;
+             }
+             else{
+                 casosAbierto++;
+             }
+     
+    });
+    console.log(TodosLoscasos);
+  
+  
+  });
+
+  
--- js/brigadistas.js
+++ js/brigadistas.js
@@ -1,72 +1,106 @@
 
-
+/**
+ * Find in array an object
+ * @param {*} myArray Arreglo en el cual se va a buscar
+ * @param {*} searchTerm  Valor del atributo
+ * @param {*} property Propiedad del Array
+ */
 function arrayObjectIndexOf(myArray, searchTerm, property) {
-  for(var i = 0, len = myArray.length; i < len; i++) {
-      if (myArray[i][property] === searchTerm) return i;
+  for (var i = 0, len = myArray.length; i < len; i++) {
+    if (myArray[i][property] === searchTerm) return i;
   }
   return -1;
 }
- 
- var database=firebase.database();
-  var referenciajeffrey = firebase.database().ref('Usuarios');
-  var starCountRef = firebase.database().ref('Usuarios');
- var Brigadistap;
- var Markers=[];
-  class Brigadista {
-    constructor(Param) {
-     var  Data=Param.val()
-      this.Nombre=Data.Nombre;
-      this.Lat= Data.Lat;
-      this.Long=Data.Long;
-      this.Estado = Data.Estado;
-      this.Cedula=Data.Cedula;
-      this.Celular=Data.Celular;
-      this.Img_Perfil=Data.Img_Perfil;
-     
-    }
-    putMarker(){
-      
-         var marker=  this.Marker=new google.maps.Marker({
-            position: {lat:this.Lat,lng:this.Long},
-            map: map,
-            Nombre:this.Nombre,
-            Estado:this.Estado,
-            Cedula:this.Cedula,
-            icon:"imagenes/policeman-standing-up.png"
-          });
-          var contentString="Nombre "+this.Nombre +"Cedula"+this.Cedula;
-          var infowindow = new google.maps.InfoWindow({
-            content: contentString
-          });
-          marker.addListener('click', function() {
-           console.log("Marcador llamado "+this.Nombre);
-           infowindow.open(map, marker);
-          });
-          return marker; 
 
-    }
+var database = firebase.database();
+var referenciajeffrey = firebase.database().ref('Usuarios');
+var starCountRef = firebase.database().ref('Usuarios');
+var Brigadistap;
+var Markers = [];
 
-    }
 
-  
-  referenciajeffrey.once('value', function( data) {
+//Clase Brigadistas
+/**
+ * Objeto Brigadista
+ */
+class Brigadista {
+  constructor(Param) {
+    var Data = Param.val()
+    this.Nombre = Data.Nombre;
+    this.Lat = Data.Lat;
+    this.Long = Data.Long;
+    this.Estado = Data.Estado;
+    this.Cedula = Data.Cedula;
+    this.Celular = Data.Celular;
+    this.Img_Perfil = Data.Img_Perfil;
+    // var storage = firebase.storage().ref("/Images/"+this.Cedula);
 
-    data.forEach(element => {
-        Markers.push(new Brigadista(element).putMarker());
+  }
+  putMarker() {
+
+    var marker = this.Marker = new google.maps.Marker({
+      position: { lat: this.Lat, lng: this.Long },
+      map: map,
+      Nombre: this.Nombre,
+      Estado: this.Estado,
+      Cedula: this.Cedula,
+      icon: "imagenes/policeman-standing-up.png"
     });
 
+    var tabla =' <link rel="stylesheet" href="css/styles.css">'+ '<table class="tg">' +
+      ' <tr><th class="tg-ikm8" rowspan="4">Fotooooo</th> <th class="tg-cobo">Nombre</th><th class="tg-6nqv">name</th>' +
+      '</tr> <tr><td class="tg-i3y8">Estado</td><td class="tg-6nqv">estado</td>' +
+      '</tr><tr><td class="tg-i3y8">Turno</td><td class="tg-6nqv">tunooo</td></tr><tr>' +
+      '<td class="tg-i3y8">fecha</td>' +
+      ' <td class="tg-6nqv">hoy</td>' +
+      ' </tr>' +
+      '</table>';
 
+    var infowindow = new google.maps.InfoWindow({
+      content: tabla,
+      disableAutoPan: true
+
+
+    });
+    marker.addListener('click', function () {
+      //Debug
+      //console.log("Marcador llamado " + this.Nombre);
+      infowindow.open(map, marker);
+    });
+    return marker;
+
+  }
+
+}
+
+
+//Carga los datos la primera vez desde RealTimeDataBase
+referenciajeffrey.once('value', function (data) {
+
+  data.forEach(element => {
+    Markers.push(new Brigadista(element).putMarker());
   });
 
-  starCountRef.on("child_changed",function(res){
 
-        var Cambio=new Brigadista(res);
-        var index=arrayObjectIndexOf(Markers,Cambio.Cedula,"Cedula");
-        console.log(index);
-      //Cambia La posición del marcador que cambio
-        Markers[index].setPosition({lat:Cambio.Lat,lng:Cambio.Long})
-   
-  });
+});
 
 
+starCountRef.on("child_changed", function (res) {
+  //Crea el objeto tipo brigasdista
+  var Cambio = new Brigadista(res);
+  //Find in Array 
+  var index = arrayObjectIndexOf(Markers, Cambio.Cedula, "Cedula");
+  arrayObjectIndexOf()
+  //Debug
+  console.log(index);
+  //Cambia La posición del marcador que cambio
+  Markers[index].setPosition({ lat: Cambio.Lat, lng: Cambio.Long })
 
+});
+
+
+
+
+
+
+var storage = firebase.storage().ref("/Images/1065.jpg");
